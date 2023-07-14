e598ad3e416a1f2ab5436cce68c50769db685fb6
Gustavo-Sander <gustavosander@ienh.com.br>
2023-07-14 10:59:46 -0300

    php arrumado

--- Index_s/cliente.php
+++ Index_s/cliente.php
@@ -4,7 +4,7 @@ if (!$conn) {
     die('Could not connect: ');
 }
 
-$sql = 'INSERT INTO cliente(nome_cliente, email, celular, senha, sexo, data_nascimento, cidade, estado, endereço) values("' . $_POST["name"] . '","' . $_POST["email"] . '","' . $_POST["telefone"] . '","' . $_POST["senha"] . '","' . $_POST["sexo"] . '","' . $_POST["date_nas"] . '","' . $_POST["city"] . '","' . $_POST["state"] . '","' . $_POST["addres"] . '")';
+$sql = 'INSERT INTO cliente(nome_cliente, email, celular, senha, sexo, data_nascimento, cidade, estado, endereco) values("' . $_POST["name"] . '","' . $_POST["email"] . '","' . $_POST["telefone"] . '","' . $_POST["senha"] . '","' . $_POST["sexo"] . '","' . $_POST["date_nas"] . '","' . $_POST["city"] . '","' . $_POST["state"] . '","' . $_POST["addres"] . '")';
 
 if ($conn->query($sql) === TRUE) {
     ?><script>window.alert("Enviado com sucesso as informações do formulário!")</script><?php
